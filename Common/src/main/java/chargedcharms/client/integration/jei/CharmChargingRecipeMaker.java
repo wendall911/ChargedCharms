2c8c4b6d1c19d8f4a5575da06614af1bd6e3a410
Wendall Cada <wendallc@83864.com>
2022-10-01 15:11:11 -0700

    Add JEI integration.

---
+++ Common/src/main/java/chargedcharms/client/integration/jei/CharmChargingRecipeMaker.java
@@ -0,0 +1,80 @@
+package chargedcharms.client.integration.jei;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import com.google.common.collect.Lists;
+import com.mojang.datafixers.util.Pair;
+
+import net.minecraft.core.NonNullList;
+import net.minecraft.core.Registry;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.CraftingRecipe;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.crafting.ShapelessRecipe;
+
+import chargedcharms.common.TagManager;
+import chargedcharms.common.item.ChargedCharmsItems;
+import chargedcharms.data.integration.ModIntegration;
+import chargedcharms.platform.Services;
+
+import static chargedcharms.util.ResourceLocationHelper.prefix;
+
+public class CharmChargingRecipeMaker {
+
+    public static List<CraftingRecipe> createRecipes() {
+        String group = "jei.charm.charging";
+        List<CraftingRecipe> recipes = new ArrayList<>();
+        List<ItemStack> regenFoods = Lists.newArrayList();
+        List<ItemStack> absorptionFoods = Lists.newArrayList();
+
+        Registry.ITEM.stream()
+                .filter(Item::isEdible)
+                .filter(item -> {
+                    List<Pair<MobEffectInstance, Float>> effects = Objects.requireNonNull(item.getFoodProperties()).getEffects();
+                    if (!effects.isEmpty()) {
+                        return effects.stream().noneMatch(props -> props.getFirst().getEffect().equals(MobEffects.POISON));
+                    }
+
+                    return true;
+                }).forEach(food -> {
+                    ItemStack foodStack = new ItemStack(food);
+
+                    if (food.getFoodProperties().getNutrition() > 4) {
+                        absorptionFoods.add(foodStack);
+                    }
+
+                    regenFoods.add(foodStack);
+                });
+
+        recipes.add(getRecipe(group, ".regen", ChargedCharmsItems.regenerationCharm, Ingredient.of(regenFoods.stream())));
+        recipes.add(getRecipe(group, ".absorption", ChargedCharmsItems.absorptionCharm, Ingredient.of(absorptionFoods.stream())));
+        recipes.add(getRecipe(group, ".totem", ChargedCharmsItems.totemCharm, Ingredient.of(Items.TOTEM_OF_UNDYING)));
+
+        if (Services.PLATFORM.isModLoaded(ModIntegration.BMO_MODID)) {
+            recipes.add(getRecipe(group, ".enchanted_totem", ChargedCharmsItems.enchantedTotemCharm, Ingredient.of(TagManager.Items.ENCHANTED_TOTEMS)));
+        }
+
+        return recipes;
+    }
+
+    private static CraftingRecipe getRecipe(String group, String label, Item charm, Ingredient chargeItems) {
+        ItemStack unchargedCharm = new ItemStack(charm);
+        unchargedCharm.setDamageValue(unchargedCharm.getMaxDamage());
+
+        Ingredient baseCharm = Ingredient.of(unchargedCharm);
+
+        ItemStack chargedCharm = unchargedCharm.copy();
+        chargedCharm.setDamageValue(chargedCharm.getMaxDamage() - 1);
+
+        NonNullList<Ingredient> chargedCharmInputs = NonNullList.of(Ingredient.EMPTY, baseCharm, chargeItems);
+
+        return new ShapelessRecipe(prefix(group + label), group, chargedCharm, chargedCharmInputs);
+    }
+
+}
---
+++ Common/src/main/java/chargedcharms/client/integration/jei/JEIPlugin.java
@@ -0,0 +1,81 @@
+package chargedcharms.client.integration.jei;
+
+import java.util.Collections;
+import java.util.IdentityHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.constants.RecipeTypes;
+import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.registration.IRecipeRegistration;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.CraftingRecipe;
+import net.minecraft.world.item.crafting.RecipeManager;
+import net.minecraft.world.item.crafting.RecipeType;
+
+import chargedcharms.ChargedCharms;
+import chargedcharms.common.crafting.recipe.AbsorptionChargeRecipe;
+import chargedcharms.common.item.ChargedCharmsItems;
+import chargedcharms.data.integration.ModIntegration;
+import chargedcharms.platform.Services;
+
+import static chargedcharms.util.ResourceLocationHelper.prefix;
+
+@JeiPlugin
+public class JEIPlugin implements IModPlugin {
+
+    @Override
+    public ResourceLocation getPluginUid() {
+        return prefix("jei_plugin");
+    }
+
+    @Override
+    public void registerRecipes(IRecipeRegistration registration) {
+        Minecraft minecraft = Minecraft.getInstance();
+        RecipeManager recipeManager = Objects.requireNonNull(minecraft.level).getRecipeManager();
+        List<CraftingRecipe> allCraftingRecipes = recipeManager.getAllRecipesFor(RecipeType.CRAFTING);
+        List<CraftingRecipe> charmChargingRecipes = addChargingRecipes(allCraftingRecipes);
+
+        registration.addRecipes(RecipeTypes.CRAFTING, charmChargingRecipes);
+        if (!Services.PLATFORM.isModLoaded(ModIntegration.BMO_MODID)) {
+            registration.getIngredientManager().removeIngredientsAtRuntime(VanillaTypes.ITEM_STACK,
+                    Collections.singleton(new ItemStack(ChargedCharmsItems.enchantedTotemCharm)));
+        }
+    }
+
+    private static List<CraftingRecipe> addChargingRecipes(List<CraftingRecipe> allCraftingRecipes) {
+        Map<Class<? extends CraftingRecipe>, Supplier<List<CraftingRecipe>>> replacers = new IdentityHashMap<>();
+
+        replacers.put(AbsorptionChargeRecipe.class, CharmChargingRecipeMaker::createRecipes);
+
+        return allCraftingRecipes.stream()
+                .map(CraftingRecipe::getClass)
+                .distinct()
+                .filter(replacers::containsKey)
+                .limit(replacers.size())
+                .flatMap(recipeClass -> {
+                    Supplier<List<CraftingRecipe>> supplier = replacers.get(recipeClass);
+
+                    try {
+                        List<CraftingRecipe> results = supplier.get();
+
+                        return results.stream();
+                    }
+                    catch (RuntimeException e) {
+                        ChargedCharms.LOGGER.error("Failed to create JEI Recipes for " + recipeClass + " " + e);
+
+                        return Stream.of();
+                    }
+                })
+                .toList();
+    }
+
+}
