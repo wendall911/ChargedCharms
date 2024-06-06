46f4cb008adffdf10721037fbbaf64b0067faea7
Wendall Cada <wendallc@83864.com>
2024-06-06 11:27:29 -0700

    Remove unused import.

--- Fabric/src/main/java/chargedcharms/FabricConfigInitializer.java
+++ Fabric/src/main/java/chargedcharms/FabricConfigInitializer.java
@@ -2,8 +2,6 @@ package chargedcharms;
 
 import com.illusivesoulworks.spectrelib.config.SpectreConfigInitializer;
 
-import chargedcharms.data.recipe.ConfigResourceCondition;
-
 public class FabricConfigInitializer implements SpectreConfigInitializer {
 
     @Override
