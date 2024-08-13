2dc6ec7420f343e3a0e842ba60f22bf10f935a74
Wendall Cada <wendallc@83864.com>
2024-08-13 11:11:02 -0700

    Defer config init. Fixes #9

--- Fabric/src/main/java/chargedcharms/FabricConfigInitializer.java
+++ Fabric/src/main/java/chargedcharms/FabricConfigInitializer.java
@@ -2,11 +2,19 @@ package chargedcharms;
 
 import com.illusivesoulworks.spectrelib.config.SpectreConfigInitializer;
 
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+
+import chargedcharms.config.ConfigHandler;
+
 public class FabricConfigInitializer implements SpectreConfigInitializer {
 
     @Override
     public void onInitializeConfig() {
         ChargedCharms.init();
+
+        ServerLifecycleEvents.SERVER_STARTING.register(server -> {
+            ConfigHandler.init();
+        });
     }
 
 }
