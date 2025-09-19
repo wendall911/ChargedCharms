34bff4638d34c0beb365cfb5d6ab01dbedc6e135
Wendall Cada <wendallc@83864.com>
2025-09-19 09:52:40 -0700

    Move Curios Tag Provider to Common data.

--- Forge/src/main/java/chargedcharms/data/ForgeDatagenInitializer.java
+++ Forge/src/main/java/chargedcharms/data/ForgeDatagenInitializer.java
@@ -1,11 +1,7 @@
 package chargedcharms.data;
 
-import java.util.Collections;
-
 import net.minecraft.data.DataGenerator;
 
-import net.minecraftforge.common.data.ExistingFileHelper;
-import net.minecraftforge.common.data.ForgeBlockTagsProvider;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
@@ -19,10 +15,7 @@ public class ForgeDatagenInitializer {
     @SubscribeEvent
     public static void configureForgeDatagen(GatherDataEvent event) {
         DataGenerator gen = event.getGenerator();
-        ExistingFileHelper disabledHelper = new ExistingFileHelper(Collections.emptyList(), Collections.emptySet(), false, null, null);
-        ForgeBlockTagsProvider blockTagsProvider = new ForgeBlockTagsProvider(gen, disabledHelper);
 
-        gen.addProvider(new ForgeItemTagProvider(gen, blockTagsProvider, ChargedCharms.MODID, disabledHelper));
         gen.addProvider(new ForgeRecipeProvider(gen));
     }
 
