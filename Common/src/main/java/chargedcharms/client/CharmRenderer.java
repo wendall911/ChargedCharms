8e14e8e356c06e7211234f61520098f9b6792d09
Wendall Cada <wendallc@83864.com>
2025-10-18 09:07:09 -0700

    1.21.10 updates for Accessories.

--- Common/src/main/java/chargedcharms/client/CharmRenderer.java
+++ Common/src/main/java/chargedcharms/client/CharmRenderer.java
@@ -2,19 +2,19 @@ package chargedcharms.client;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 
+import io.wispforest.accessories.api.client.AccessoryRenderState;
 import io.wispforest.accessories.api.client.renderers.AccessoryRenderer;
 import io.wispforest.accessories.api.client.renderers.SimpleAccessoryRenderer;
-import io.wispforest.accessories.api.slot.SlotPath;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.model.HumanoidModel;
-import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.entity.state.HumanoidRenderState;
 import net.minecraft.client.renderer.entity.state.LivingEntityRenderState;
+import net.minecraft.client.renderer.item.ItemStackRenderState;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
 
 import chargedcharms.config.ConfigHandler;
@@ -22,21 +22,18 @@ import chargedcharms.config.ConfigHandler;
 public class CharmRenderer implements SimpleAccessoryRenderer {
 
     @Override
-    public <S extends LivingEntityRenderState> void render(ItemStack stack, SlotPath path, PoseStack poseStack,
-               EntityModel<S> model, S renderState, MultiBufferSource buffer, int light, float partialTicks) {
+    public <S extends LivingEntityRenderState> void renderStack(AccessoryRenderState accessoryState,
+            S entityState, EntityModel<S> model, PoseStack poseStack, SubmitNodeCollector collector,
+            ItemStack stack, ItemStackRenderState stackRenderState, int light) {
         if (ConfigHandler.Client.showCharms()) {
-            Minecraft mc = Minecraft.getInstance();
-
-            align(stack, path, model, renderState, poseStack);
             poseStack.scale(0.45F, 0.45F, 0.45F);
-            mc.getItemRenderer().renderStatic(stack, ItemDisplayContext.GUI, light, OverlayTexture.NO_OVERLAY,
-                    poseStack, buffer, mc.level, 0);
+            stackRenderState.submit(poseStack, collector, light, OverlayTexture.NO_OVERLAY, 0);
         }
     }
 
     @Override
-    public <S extends LivingEntityRenderState> void align(ItemStack stack, SlotPath path, EntityModel<S> model,
-              S renderState, PoseStack poseStack) {
+    public <S extends LivingEntityRenderState> void align(AccessoryRenderState accessoryRenderState, S entityState,
+            EntityModel<S> model, PoseStack poseStack) {
         if ((model instanceof HumanoidModel<? extends HumanoidRenderState> humanoidModel)) {
             Minecraft mc = Minecraft.getInstance();
             float zPos = 1.1F;
